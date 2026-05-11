92f3e9195c4d57a056a1121049f1e06c8cc6394e
epicsagas <wowinno@gmail.com>
2026-05-11 11:26:33 +0900

    feat: add obsidian-forge 0.1.6 formula

---
+++ Formula/obsidian-forge.rb
@@ -0,0 +1,25 @@
+class ObsidianForge < Formula
+  desc "Obsidian vault generator, automation daemon, and graph strengthener"
+  homepage "https://github.com/epicsagas/obsidian-forge"
+  version "0.1.6"
+  license "Apache-2.0"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/epicsagas/obsidian-forge/releases/download/v0.1.6/obsidian-forge-aarch64-apple-darwin.tar.gz"
+      sha256 "6cdc1c31d6d35493f1d50057fc1f3347f943c8a28a093790cce64bab92de681d"
+    end
+    on_intel do
+      url "https://github.com/epicsagas/obsidian-forge/releases/download/v0.1.6/obsidian-forge-x86_64-apple-darwin.tar.gz"
+      sha256 "10c594d7419ef4cfb8124e8b5b940086924560ddf6095c3307614c91700b674d"
+    end
+  end
+
+  def install
+    bin.install "obsidian-forge"
+  end
+
+  test do
+    assert_match "obsidian-forge", shell_output("#{bin}/obsidian-forge --version 2>&1")
+  end
+end
