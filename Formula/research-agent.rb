ccf82531c9ba537f03d7562b8239513b248dcb09
epicsagas <39856742+epicsagas@users.noreply.github.com>
2026-09-11 15:47:51 +0900

    research-agent 0.2.0: rebuild hashes

--- Formula/research-agent.rb
+++ Formula/research-agent.rb
@@ -5,32 +5,32 @@ class ResearchAgent < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/epicsagas/research-agent/releases/download/v0.2.0/research-agent-aarch64-apple-darwin.tar.xz"
-      sha256 "d5dd382e00890a0c4e77dd0b7f6986373fd255be5dc33f186985e3f0088a8496"
+      sha256 "ba59f0d7c907d4793f675cb4cb9de8bd0fdf639ac463fc2d9e68daf0c0695d93"
     end
     if Hardware::CPU.intel?
       url "https://github.com/epicsagas/research-agent/releases/download/v0.2.0/research-agent-x86_64-apple-darwin.tar.xz"
-      sha256 "a6bb9b2a0c40e1c73b699ecaee10dcecf4e1ac83bf121a81b333f058a8f1389f"
+      sha256 "d47028b2c0d05ac357732496549c4641de96fb07584f3c7320683a2bfc03292d"
     end
   end
   if OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/epicsagas/research-agent/releases/download/v0.2.0/research-agent-aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "77a590c3fa5a83536ca47f339d807ac607dc6def59c6ba76049bfdbb6d72ddae"
+      sha256 "a958305fc9018cbd6f56732867e8b3197ceaa8a4c2ce93e6c1bba7355168216e"
     end
     if Hardware::CPU.intel?
       url "https://github.com/epicsagas/research-agent/releases/download/v0.2.0/research-agent-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "57c75f0cd487d26bf03431de3ac54af590aa939ea16ec49d4474253be1eb3081"
+      sha256 "b53cb57fc56d7438556c4aca4335b2220636b7d3ad4f8c3ee2f947d7b680080d"
     end
   end
   license "Apache-2.0"
 
   BINARY_ALIASES = {
-    "aarch64-apple-darwin":      {},
+    "aarch64-apple-darwin": {},
     "aarch64-unknown-linux-gnu": {},
-    "x86_64-apple-darwin":       {},
-    "x86_64-pc-windows-gnu":     {},
-    "x86_64-unknown-linux-gnu":  {},
-  }.freeze
+    "x86_64-apple-darwin": {},
+    "x86_64-pc-windows-gnu": {},
+    "x86_64-unknown-linux-gnu": {}
+  }
 
   def target_triple
     cpu = Hardware::CPU.arm? ? "aarch64" : "x86_64"
